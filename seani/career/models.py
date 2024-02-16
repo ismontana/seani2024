df316c05d7133039cf9239357c940e7519fb938c
ismontana <ismaotsss@gmail.com>
2024-02-16 09:55:01 -0600

    03 App Career Model Career

---
+++ seani/career/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+class Career(models.Model):
+
+    LEVELS = [
+        ('Ing', 'Ingenierría'),
+        ('TSU', 'Tecnico Superior Universitario'),
+        ('M', 'Maestría')
+    ]
+
+    level = models.CharField(
+        verbose_name = 'Nivel',
+        max_length = 10, 
+        choices = LEVELS)
+    name = models.CharField(
+        verbose_name = 'Nombre',
+        max_length = 200
+        )
+    short_name = models.CharField(
+        verbose_name = 'Abreviatura',
+        max_length = 20
+        )
+    
+    def __str__(self):
+        return f"{ self.level } - {self.short_name}"
+    
+    class Meta:
+        verbose_name = 'carrera'
+        verbose_name_plural = 'carreras'
--- seani/seani/settings.py
+++ seani/seani/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # Local Apps
     'library.apps.LibraryConfig',
+    'career.apps.CareerConfig',
     #Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
